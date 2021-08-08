696afc7bc157a26158d2e6c1b0127203626befa6
Tim Yuen <timothy.yuen22@gmail.com>
2021-08-08 01:24:55 -0400

    Make GLTF module togglable when compiling release templates

--- modules/gltf/gltf_document.cpp
+++ modules/gltf/gltf_document.cpp
@@ -53,7 +53,9 @@
 #include "core/version.h"
 #include "core/version_hash.gen.h"
 #include "drivers/png/png_driver_common.h"
+#ifdef TOOLS_ENABLED
 #include "editor/import/resource_importer_scene.h"
+#endif
 #include "scene/2d/node_2d.h"
 #include "scene/3d/bone_attachment.h"
 #include "scene/3d/camera.h"
